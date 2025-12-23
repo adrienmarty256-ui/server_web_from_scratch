dce2f21a6d8a9de314c2e8351114e618260dcc22
adrien_deb <adrien@2a02-8440-7514-dd17-7add-08ff-feb9-222c.rev.sfr.net>
2025-12-23 16:54:47 +0100

    server web from scratch

---
+++ server_web_C#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System;
+
+class PortListener
+{
+    int port = 5000;
+    IPAddress localserver = IPAddress.Loopback;
+    TcpListener serverWeb;
+
+    public PortListener()
+    {
+        serverWeb = new TcpListener(localserver, port);
+    }
+
+    public void Start()
+    {
+        serverWeb.Start();
+    }
+
+    public TcpClient AcceptTcpClient()
+    {
+        return serverWeb.AcceptTcpClient();
+    }
+
+    public static void Main()
+    {
+        PortListener portListener = new PortListener();
+        portListener.Start();
+
+        while(true)
+        {
+            Console.Write("Waiting for a connection... ");
+            TcpClient client = portListener.AcceptTcpClient();
+            Console.WriteLine("Connected!");
+        }
+      
+    }
+
+}
+
