97a9ab2b8498a6beefdcd53a290d2378fa4d4277
adrien_deb <adrien@2a02-8440-7514-dd17-7add-08ff-feb9-222c.rev.sfr.net>
2025-12-23 19:50:04 +0100

    Etape 2 : ajout du routage HTTP

--- server_web_C#/Program.cs
+++ server_web_C#/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Sockets;
+﻿using System;
 using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 class PortListener
 {
@@ -15,6 +17,7 @@ class PortListener
     public void Start()
     {
         serverWeb.Start();
+        Console.WriteLine($"Server started on {localserver}:{port}");
     }
 
     public TcpClient AcceptTcpClient()
@@ -29,12 +32,43 @@ class PortListener
 
         while(true)
         {
-            Console.Write("Waiting for a connection... ");
+            Console.WriteLine("Waiting for a connection...");
             TcpClient client = portListener.AcceptTcpClient();
             Console.WriteLine("Connected!");
-        }
-      
-    }
 
+            // Lecture de la requête HTTP
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            string requestLine = reader.ReadLine();
+            Console.WriteLine($"Request: {requestLine}");
+
+            // Extraire le chemin demandé
+            string path = "/";
+            if (!string.IsNullOrEmpty(requestLine))
+            {
+                string[] tokens = requestLine.Split(' ');
+                if (tokens.Length >= 2)
+                    path = tokens[1];
+            }
+
+            // Préparer la réponse selon le chemin
+            string responseText = path switch
+            {
+                "/" => "Bienvenue sur la page d'accueil !",
+                "/hello" => "Hello world depuis le serveur C#!",
+                _ => "404 - Page non trouvée"
+            };
+
+            // Envoyer la réponse HTTP
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine("HTTP/1.1 200 OK");
+            writer.WriteLine("Content-Type: text/plain");
+            writer.WriteLine();
+            writer.WriteLine(responseText);
+            writer.Flush();
+
+            client.Close();
+        }
+    }
 }
 
